4c64bba49084532cf440efc0f557ed8bc8a9612a
riccelle011 <jr7263857@gmail.com>
2024-03-18 09:55:46 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alert);
+var nome = document.getElementById("nome");
+
+var saida = document.getElementById("saida-de-dados")
+
+var email = document.getElementById("email");
 
 function alertar(){
-    alert("Você clicou no botão!!!");
+    //alert("nome.value + " " + "clicou no botão!!!");
+    saida.innerText = "Nome: " + " " + nome.value +
+                "\n E-mail: " + " " + email.value;
 }
