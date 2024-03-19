c0e68e0476b544dae4dceb5f34deb8a7d63b6ecc
riccelle011 <jr7263857@gmail.com>
2024-03-19 09:15:16 -0300

    busca cep

--- scripts.js
+++ scripts.js
@@ -10,4 +10,15 @@ function alertar(){
     //alert("nome.value + " " + "clicou no botão!!!");
     saida.innerText = "Nome: " + " " + nome.value +
                 "\n E-mail: " + " " + email.value;
+
+                //buscar o endereço pelo cep
+                const url = https: //´viacep.com.br/ws/${cep.value}/json´;
+
+                fetch(url)
+                .then(resposta=>resposta.json())
+                .then(data => {
+                    logradouro.value = data.logradouro;
+                })
+                .catch(error=>alert(error))               
+}
 }
